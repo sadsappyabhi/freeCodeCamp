eb3196c495132d1272353fb186a7d2db14e6aedb
abhi <ajayanthi2@gmail.com>
2022-12-22 01:05:51 -0500

    fix: updated type_id in properties table to pass final test

--- periodic_table.sql
+++ periodic_table.sql
@@ -66,7 +66,7 @@ CREATE TABLE public.properties (
     atomic_mass numeric NOT NULL,
     melting_point_celsius numeric NOT NULL,
     boiling_point_celsius numeric NOT NULL,
-    type_id integer
+    type_id integer NOT NULL
 );
 
 
@@ -134,17 +134,17 @@ INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.properties VALUES (1, 'nonmetal', 1.008, -259.1, -252.9, NULL);
-INSERT INTO public.properties VALUES (2, 'nonmetal', 4.0026, -272.2, -269, NULL);
-INSERT INTO public.properties VALUES (3, 'metal', 6.94, 180.54, 1342, NULL);
-INSERT INTO public.properties VALUES (4, 'metal', 9.0122, 1287, 2470, NULL);
-INSERT INTO public.properties VALUES (5, 'metalloid', 10.81, 2075, 4000, NULL);
-INSERT INTO public.properties VALUES (6, 'nonmetal', 12.011, 3550, 4027, NULL);
-INSERT INTO public.properties VALUES (7, 'nonmetal', 14.007, -210.1, -195.8, NULL);
-INSERT INTO public.properties VALUES (8, 'nonmetal', 15.999, -218, -183, NULL);
-INSERT INTO public.properties VALUES (1000, 'metalloid', 1, 10, 100, NULL);
+INSERT INTO public.properties VALUES (1, 'nonmetal', 1.008, -259.1, -252.9, 1);
+INSERT INTO public.properties VALUES (2, 'nonmetal', 4.0026, -272.2, -269, 1);
+INSERT INTO public.properties VALUES (6, 'nonmetal', 12.011, 3550, 4027, 1);
+INSERT INTO public.properties VALUES (7, 'nonmetal', 14.007, -210.1, -195.8, 1);
+INSERT INTO public.properties VALUES (8, 'nonmetal', 15.999, -218, -183, 1);
 INSERT INTO public.properties VALUES (9, 'nonmetal', 18.998, -220, -188.1, 1);
 INSERT INTO public.properties VALUES (10, 'nonmetal', 20.18, -248.6, -246.1, 1);
+INSERT INTO public.properties VALUES (3, 'metal', 6.94, 180.54, 1342, 2);
+INSERT INTO public.properties VALUES (4, 'metal', 9.0122, 1287, 2470, 2);
+INSERT INTO public.properties VALUES (5, 'metalloid', 10.81, 2075, 4000, 3);
+INSERT INTO public.properties VALUES (1000, 'metalloid', 1, 10, 100, 3);
 
 
 --
