489c7e7e3e25f37715ad61d89563565305990fb6
abhi <ajayanthi2@gmail.com>
2022-12-24 17:11:48 -0500

    fix: fixed test for element not found. Script now passes all tests

--- element.sh
+++ element.sh
@@ -8,29 +8,50 @@ then
 fi
 
 
-if [[ $1 =~ ^[0-9]$ ]]
+if [[ $1 =~ ^[0-9]{1,3}$ ]]
 then
-	ELEM_BY_NUM=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1 ")
-	echo $ELEM_BY_NUM | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT
-	do
-		echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-	done
+	NULL=$($PSQL "SELECT COUNT(atomic_number) FROM elements WHERE atomic_number = $1")
+
+	if [[ $NULL -eq 0 ]]
+	then
+		echo "I could not find that element in the database."
+	else
+		ELEM_BY_NUM=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1 ")
+		echo $ELEM_BY_NUM | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT
+			do
+				echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+			done
+	fi
 fi
 
 if [[ $1 =~ ^[A-Za-z]{1,2}$ ]]
 then
-	ELEM_BY_SYM=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol = '$1'")
-	echo $ELEM_BY_SYM | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT
-	do
-		echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-	done
+	NULL=$($PSQL "SELECT COUNT(symbol) FROM elements WHERE symbol = '$1'")
+
+	if [[ $NULL -eq 0 ]]
+	then
+		echo "I could not find that element in the database."
+	else
+		ELEM_BY_SYM=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol = '$1'")
+		echo $ELEM_BY_SYM | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT
+		do
+			echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+		done
+	fi
 else
 if [[ $1 =~ ^[A-Za-z]+$ ]]
 then
-	ELEM_BY_NAME=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE name = '$1'")
-	echo $ELEM_BY_NAME | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT
-	do
-		echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-	done
+	NULL=$($PSQL "SELECT COUNT(name) FROM elements WHERE name = '$1'")
+	if [[ $NULL -eq 0 ]]
+	then
+		echo "I could not find that element in the database."
+	else
+		ELEM_BY_NAME=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE name = '$1'")
+		echo $ELEM_BY_NAME | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT
+		do
+			echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+		done
+	fi
 fi
 fi
+
